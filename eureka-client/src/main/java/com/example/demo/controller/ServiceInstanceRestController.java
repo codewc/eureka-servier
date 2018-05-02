dfabac1d4eb0cf9754a17eaa925ac23321763f26
wangchun <wangchun@DESKTOP-F0028ND>
2018-05-02 22:38:31 +0800

    系统优化

--- eureka-client/src/main/java/com/example/demo/controller/ServiceInstanceRestController.java
+++ eureka-client/src/main/java/com/example/demo/controller/ServiceInstanceRestController.java
@@ -2,34 +2,26 @@ package com.example.demo.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
+import javax.annotation.Resource;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.dao.GenericDao;
 import com.example.demo.entity.Person;
-
-import lombok.extern.slf4j.Slf4j;
+import com.example.demo.service.PersonService;
 
 @RestController
 @RequestMapping("/db")
-@Slf4j
 public class ServiceInstanceRestController {
 
-	@Lazy(true)
-	@Autowired
-	private GenericDao<Person, Long> basicDaoImpl;
-
+	@Resource
+	PersonService personService;
+	
 	@RequestMapping("/query")
 	@ResponseBody
 	public List<Person> home(@RequestParam(name = "name") String name) {
-		List<Person> personList = basicDaoImpl.find("from Person");
-		for (Person person : personList) {
-			log.info(person.getName() + person.getAge());
-		}
-		return personList;
+		return personService.find(name);
 	}
 }
